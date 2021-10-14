6a8d430a7cc6e157d6f986baab9a42aa058f04d0
Pastuszka <michal.pastuszka98@gmail.com>
2021-10-14 17:11:41 +0200

    fix test case

--- test_needleman_wunsch.py
+++ test_needleman_wunsch.py
@@ -13,14 +13,15 @@ class TestNeedlemanWunsch(unittest.TestCase):
     def test_align(self):
         seq_a = 'CATAC'
         seq_b = 'ATCGAC'
+        solver = NeedlemanWunsch()
         correct_alignment = np.array([[0, -4, -8, -12, -16, -20, -24],
                                       [-4, -3, -7, -3, -7, -11, -15],
                                       [-8, 1, -3, -7, -6, -2, -6],
                                       [-12, -3, 6, 2, -2, -6, -5],
                                       [-16, -7, 2, 3, -1, 3, -1],
                                       [-20, -11, -2, -1, 0, -1, 8]])
-        alignment_matrix, arrow_matrix = NeedlemanWunsch.__align__(seq_a, seq_b)
-        self.assertEqual(alignment_matrix, correct_alignment)
+        alignment_matrix, arrow_matrix = solver.__align__(seq_a, seq_b)
+        self.assertTrue(np.array_equal(alignment_matrix, correct_alignment))
 
 
 if __name__ == '__main__':
