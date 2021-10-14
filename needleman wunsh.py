f1f18ae88e340ef3fb35e00ffdee1b5d4d3b4eef
Pastuszka <michal.pastuszka98@gmail.com>
2021-10-14 14:51:59 +0200

    Initial commit

---
+++ needleman wunsh.py
@@ -0,0 +1,3 @@
+
+class NeedlemanWunsch:
+    pass
---
+++ test_needleman_wunsch.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestNeedlemanWunsch(unittest.TestCase):
+
+    def test_align(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
